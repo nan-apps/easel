95aa0745ee7249b9c58aaa811b030c78ff1bcc14
talvbansal <talvbansal@hotmail.com>
2017-11-20 09:47:12 +0000

    Replace Laravel JS object with csrf-token meta tag

--- resources/views/backend/shared/partials/js.blade.php
+++ resources/views/backend/shared/partials/js.blade.php
@@ -1,7 +1,2 @@
 <script type="text/javascript" src="{{ asset('vendor/canvas/assets/js/vendor.js') }}"></script>
 <script type="text/javascript" src="{{ asset('vendor/canvas/assets/js/app.js') }}"></script>
-<script type="text/javascript">
-    window.Laravel = <?php echo json_encode([
-            'csrfToken' => csrf_token(),
-    ]); ?>
-</script>
