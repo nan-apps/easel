1e179592c34f6de3194524e5ba51826e165b2271
Glenn McEwan <glenn@web-dev.ninja>
2018-01-05 01:50:40 +0000

    Remove HipChat link from admin (#165)
    Thanks @glennmcewan 👍

--- resources/views/backend/shared/partials/footer.blade.php
+++ resources/views/backend/shared/partials/footer.blade.php
@@ -5,7 +5,6 @@
         <li><a href="https://cnvs.readme.io/docs" target="_blank">Documentation</a></li>
         <li><a href="https://cnvs.readme.io/blog" target="_blank">Blog</a></li>
         <li><a href="https://github.com/cnvs/canvas" target="_blank">GitHub</a></li>
-        <li><a href="https://cnvs.hipchat.com" target="_blank">HipChat</a></li>
         <li><a href="https://cnvs.readme.io/discuss" target="_blank">Support</a></li>
     </ul>
 </footer>
